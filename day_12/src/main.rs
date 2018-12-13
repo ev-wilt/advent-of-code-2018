816ffd93a4b1d3aa7de270869136200da54afcb5
Evan Wilt <evdawilt@umail.iu.edu>
2018-12-13 16:25:31 -0500

    Finished day 12 part 2

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -46,7 +46,40 @@ fn part_one() {
     println!("{}", sum);
 }
 
+fn part_two() {
+    let mut cur_state = String::from("##.#..########..##..#..##.....##..###.####.###.##.###...###.##..#.##...#.#.#...###..###.###.#.#");
+    let states = build_map();
+    let mut left_shift: i32 = 0;
+    for _ in 0..500 as i64 {
+        while &cur_state[0..4] != "....".to_string() {
+            cur_state.insert(0, '.');
+            left_shift += 1;
+        }
+        while &cur_state[cur_state.len() - 4..cur_state.len()] != "....".to_string() {
+            cur_state.push('.');
+        }
+
+        let mut new_state = String::new();
+        for i in 0..cur_state.len() - 4 {
+            let slice = &cur_state[i..i + 5].to_string();
+            new_state.push(*states.get(slice).unwrap());
+        }
+        left_shift -= 2;
+        cur_state = new_state;
+        
+    }
+    let mut sum: i32 = 0;
+    for pot in cur_state.chars().enumerate() {
+        let (index, state) = pot;
+        if state == '#' {
+            sum += index as i32 - left_shift;
+        }
+    }
+    cur_state.retain(|c| c == '#');
+    println!("{}", ((50000000000 as i64 - 500) * cur_state.len() as i64) + sum as i64);
+}
+
 
 fn main() {
-    part_one();
+    part_two();
 }
